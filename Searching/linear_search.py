ece6a23f83e613c6f8e3a524d3bdf3fa92c92dc5
Prachi Chodankar <55678809+prachi1807@users.noreply.github.com>
2021-03-08 12:51:33 +0530

    Linear Search 2 ways: O(n) and O(n/2) time

--- Searching/linear_search.py
+++ Searching/linear_search.py
@@ -1,10 +1,32 @@
 # Linear Search
 def linear_search(arr,x):
+    # Worst case time complexity = O(n)
     for i in range(len(arr)):
         if arr[i] == x:
             return (i)
     return -1
+    
+def linear_search_1(arr,x):
+    # Worst case time complexity = O(n/2)
+    # Uses 2 pointers, one from start and the other from the end
+    # Takes half as much of the time
+    
+    left = 0
+    right = len(arr)-1
+    pos = -1
+    while left<right:
+        if arr[left] == x:
+            return left
+        
+        if arr[right] == x:
+            return right
+            
+        left += 1
+        right -= 1
+        
+    return -1
 
 arr = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170]
-x = 180
+x = 130
 print(linear_search(arr,x))
+print(linear_search_1(arr,x))
