5e351edadaa2ed7c04e23eab429af280fff158e0
Prachi Chodankar <55678809+prachi1807@users.noreply.github.com>
2021-03-16 20:10:10 +0530

    Create stack_implementation.py

---
+++ Stack/stack_implementation.py
@@ -0,0 +1,30 @@
+# Stack implementation using list
+class Stack:
+    
+    def __init__(self):
+        self.stack = []
+        self.top = -1
+        
+    def push(self, value):
+        self.stack.append(value)
+        self.top += 1
+        
+    def pop(self):
+        if self.top!=-1:
+            return self.stack.pop()
+        return -1
+    
+    def printStack(self):
+        print(self.stack)
+    
+stack = Stack()
+
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+
+stack.pop()
+stack.pop()
+
+stack.printStack()
